2172e481f536e3b07844ea06727f769e62902a3e
Alex Tomjack <amt1309@gmail.com>
2024-06-18 19:38:04 -0500

    Temporary translucent background on non-space characters. Affects performance and is not adjustable, fix later

--- spout-program/res/shaders/text.fs
+++ spout-program/res/shaders/text.fs
@@ -10,5 +10,17 @@ void main()
 {    
     //vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
     vec4 sampled= vec4(1.0, 1.0, 1.0, texture(textArray, TexCoords).r);
-    color = vec4(textColor, 1.0) * sampled;
-}  
+    //not empty space
+    if (TexCoords.z > 0.1) {
+        //black pixel
+        if(sampled.w == 0.0 ) {
+            color = vec4(128.0, 128.0, 128.0, 0.5);
+        } else {
+            //color = vec4(textColor, 1.0) * sampled;
+            color = vec4(textColor, 1.0);
+        }
+    }
+    else {
+        color = vec4(textColor, 0.0);
+    }
+}

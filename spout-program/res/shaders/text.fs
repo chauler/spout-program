0931fc56d182a9da8b02c5638574606a0696d067
Alex Tomjack <amt1309@gmail.com>
2024-07-08 14:05:23 -0500

    Fixed new texel fetch. Old luminance would go out of bounds when pixel was white.

--- spout-program/res/shaders/text.fs
+++ spout-program/res/shaders/text.fs
@@ -15,7 +15,7 @@ void main()
     //Grab the RGB from the input image
     vec4 inputSample = texelFetch(inputTexture, ivec2(vec2(gl_FragCoord.x, imgDims.y * charSize - gl_FragCoord.y) / vec2(charSize)), 0);
     //Calculate luminance for use in sampling the glyph
-    float luminance = float(inputSample.r * 0.2126 + inputSample.g * .7152 + inputSample.b * .0722) * charsetSize;
+    float luminance = float(inputSample.r * 0.2126 + inputSample.g * .7152 + inputSample.b * .0722) * (charsetSize - 1);
     //Texture access here just determines which layer of glyph array we use to determine alpha.
     ivec2 pixelPosition = ivec2(mod(gl_FragCoord.x, charSize), mod(gl_FragCoord.y, charSize)) * textureSize(textArray, 0).xy / ivec2(charSize);
     vec4 glyphSample= vec4(1.0, 1.0, 1.0, texelFetch(textArray, ivec3(pixelPosition, luminance), 0).r);

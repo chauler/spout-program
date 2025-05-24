422a9bcf43ada3f5a97e28d196903ba1c95145c9
Alex Tomjack <amt1309@gmail.com>
2025-05-24 16:30:26 -0500

    Image previews are now resizeable.

--- spout-program/src/gui/components/PreviewPanel.cpp
+++ spout-program/src/gui/components/PreviewPanel.cpp
@@ -19,11 +19,20 @@ void SpoutProgram::Gui::PreviewPanel::Render() {
         const float framerate = ImGui::GetIO().Framerate;
         ImGui::Text("size = %d x %d \t | \t %.3f ms/frame (%.1f FPS)", m_sourceTex.GetDimensions().x, m_sourceTex.GetDimensions().y, 1000.0f / framerate, framerate);
         auto contentSpace = ImGui::GetContentRegionAvail();
-        auto initialCursorPos = ImGui::GetCursorPos();
         const float gap = 5.0f;
-        const ImVec2 maxDims{ contentSpace.x / 2 - gap, contentSpace.y }; //The max space allocated for each image.
-		//Get each image to fit within the allocated space while maintaining the aspect ratio.
-		uVec2 sourceDims = m_sourceTex.GetDimensions();
+        const ImVec2 defaultDims{ contentSpace.x / 2 - gap, contentSpace.y }; //The max space allocated for each image.
+		ImVec2 parentDims = ImGui::GetWindowSize();
+        //Get each image to fit within the allocated space while maintaining the aspect ratio.
+        ImGui::SetNextWindowSizeConstraints({ parentDims.x * 0.1f, 0.0f}, {parentDims.x * 0.9f, parentDims.y});
+        ImGui::BeginChild("SourcePreview", defaultDims, ImGuiChildFlags_ResizeX);
+		ImVec2 pos = ImGui::GetWindowPos();
+		ImVec2 size = ImGui::GetWindowSize();
+		//Limit size to 90% of the available space.
+		if (size.x > parentDims.x * 0.9f) {
+			size.x = parentDims.x * 0.9f;
+		}
+		ImVec2 maxDims = ImGui::GetWindowSize();
+        uVec2 sourceDims = m_sourceTex.GetDimensions();
         ImVec2 imageDims{};
 		double scaleFactor = Util::ComputeScaleFactor(sourceDims.x, sourceDims.y, maxDims.x, maxDims.y);
         if (scaleFactor < 1.0) {
@@ -31,15 +40,20 @@ void SpoutProgram::Gui::PreviewPanel::Render() {
             imageDims.y = sourceDims.y * scaleFactor;
         }
         ImGui::Image(m_sourceTex.GetID(), imageDims);
-        ImGui::GetForegroundDrawList()->AddLine({ pos.x + initialCursorPos.x + maxDims.x + gap, initialCursorPos.y }, { pos.x + initialCursorPos.x + maxDims.x + gap, initialCursorPos.y + maxDims.y }, ImGui::GetColorU32(ImGuiCol_HeaderHovered), gap);
-        ImGui::SetCursorPos(ImVec2(initialCursorPos.x + maxDims.x + gap * 2.0f, initialCursorPos.y));
-
+		ImGui::EndChild();
+		//Draw a line between the two images. Get this child's position and size to draw the line.
+        ImGui::GetForegroundDrawList()->AddLine({ pos.x + size.x + gap/2, pos.y }, { pos.x + size.x + gap / 2, pos.y + size.y }, ImGui::GetColorU32(ImGuiCol_HeaderHovered), gap);
+        
+		ImGui::SameLine();
+		ImGui::BeginChild("OutputPreview", {0, 0});
+        maxDims = ImGui::GetWindowSize();
 		scaleFactor = Util::ComputeScaleFactor(m_outputTex.w, m_outputTex.h, maxDims.x, maxDims.y);
 		if (scaleFactor < 1.0) {
 			imageDims.x = m_outputTex.w * scaleFactor;
 			imageDims.y = m_outputTex.h * scaleFactor;
 		}
         ImGui::Image(m_outputTex.id, imageDims);
+		ImGui::EndChild();
 	}
     else {
         ImGui::Text("Select both an Input and an Output.");
